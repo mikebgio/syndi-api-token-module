ca7ab9c3b5b3fd2bed830318b85e38de71343ffc
Mike Giordano <mgiord1987@gmail.com>
2017-12-01 19:15:48 -0500

    Initial commit including completed module with sample config

---
+++ sample_config.py
@@ -0,0 +1,4 @@
+CLIENT_ID = '<Your Client ID>'
+CLIENT_SECRET = '<Your Client Secret>'
+USERNAME = '<Your Syndicaster Account Username>'
+PASSWORD = '<MD5 Hash of Your Syndicaster Account Password>'
---
+++ syndi.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+def get_token(client_id, client_secret, username, password):
+    get_token_files = {
+        'grant_type': (None, 'password'),
+        'client_id': (None, client_id),
+        'client_secret': (None, client_secret),
+        'scope': (None, 'read write'),
+        'username': (None, username),
+        'password': (None, password),
+    }
+    r = requests.post(
+        'http://api.syndicaster.tv/oauth/access_token',
+        files=get_token_files)
+    pjson = json.loads(r.text)
+    ACCESS_TOKEN = pjson['access_token']
+    REFRESH_TOKEN = pjson['refresh_token']
+    return(ACCESS_TOKEN, REFRESH_TOKEN)
+
+
+def refresh(client_id, client_secret, refresh_token):
+    refresh_token_files = {
+        'grant_type': (None, 'refresh_token'),
+        'client_id': (None, client_id),
+        'client_secret': (None, client_secret),
+        'refresh_token': (None, refresh_token)
+    }
+    r = requests.post(
+        'http://api.syndicaster.tv/oauth/access_token',
+        files=refresh_token_files)
+    pjson = json.loads(r.text)
+    ACCESS_TOKEN = pjson['access_token']
+    REFRESH_TOKEN = pjson['refresh_token']
+    return(ACCESS_TOKEN, REFRESH_TOKEN)
